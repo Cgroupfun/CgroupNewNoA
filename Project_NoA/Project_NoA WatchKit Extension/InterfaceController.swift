78349e25ac279882b4c0f8d8b6de6b29a9f8d004
keiyakato <keimusic1015@softbank.ne.jp>
2018-11-07 18:05:19 +0900

    画像の追加、激励のアニメーションの追加

--- Project_NoA/Project_NoA WatchKit Extension/InterfaceController.swift
+++ Project_NoA/Project_NoA WatchKit Extension/InterfaceController.swift
@@ -22,12 +22,13 @@ class InterfaceController: WKInterfaceController {
     }
     @IBOutlet var ZZZ: WKInterfaceLabel!
     //タッチで喋る、動く
-    @IBAction func Move(sender: AnyObject) {
-        animate(withDuration: 0.5) { () -> Void in
+    @IBAction func Move(_ sender: Any) {
+         self.ZZZ.setText("反応あり")
+    animate(withDuration: 0.5) { () -> Void in
             self.NoA.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.right)
         }
         
-        self.animate(withDuration: 1) { () -> Void in
+        self.animate(withDuration: 1.0) { () -> Void in
             self.NoA.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.left)
         }
         
@@ -35,6 +36,12 @@ class InterfaceController: WKInterfaceController {
             self.NoA.setHorizontalAlignment(WKInterfaceObjectHorizontalAlignment.center)
         }
     }
+    
+    @IBAction func ouenn() {
+        self.ZZZ.setText("反応")
+        pushController(withName: "Ouenn", context: nil)
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -47,6 +54,9 @@ class InterfaceController: WKInterfaceController {
         //充電時の画面遷移
         if WKInterfaceDevice.current().batteryState == WKInterfaceDeviceBatteryState.charging{
             self.ZZZ.setText("おやすみ")
+            
+            //NoAの寝てる画像に移動
+            presentController(withName: "Oyasumi", context: nil)
         }
        
     }
@@ -54,7 +64,20 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // アプリが起動してない状態の動作
         super.didDeactivate()
+        //iPhoneだとこれ（↓）でできる。。。
+    //NoA.animatedImage = suiminNoA()
         
+    }
+    //コマ送りのイメージの配列を作る（再生は別）
+    func suiminNoA () -> Array<UIImage> {
+        var theArray = Array<UIImage>()
+        for num in 1...4 {
+            let imageName = "AppleWatch_睡眠" + String(num)
+            let image = UIImage(named: imageName)
+            
+            theArray.append(image!)
+        }
+        return theArray
     }
 
 }
