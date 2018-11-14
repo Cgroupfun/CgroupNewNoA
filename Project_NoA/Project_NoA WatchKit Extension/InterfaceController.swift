fe319f2814f6ecc790d8a0a9bbfef05bb6710c28
keiyakato <keimusic1015@softbank.ne.jp>
2018-11-14 17:17:29 +0900

    リモートの画像取得

--- Project_NoA/Project_NoA WatchKit Extension/InterfaceController.swift
+++ Project_NoA/Project_NoA WatchKit Extension/InterfaceController.swift
@@ -16,6 +16,10 @@ class InterfaceController: WKInterfaceController {
     let engine = AVAudioEngine()
     let audioPlayerNode = AVAudioPlayerNode()
     var audioFile:AVAudioFile!
+    //画像取得のための変数2つ
+    var task: URLSessionDataTask?
+    var isActive: Bool = false
+
     //NoAの画像
     @IBOutlet var NoA: WKInterfaceImage!
     
@@ -41,25 +45,20 @@ class InterfaceController: WKInterfaceController {
     }
     
     @IBAction func ouenn() {
-
-        //musicSet(name:"se", type:"wav")
-        // オーディオファイルの再生をスケジュールする
-        self.audioPlayerNode.scheduleFile(self.audioFile!, at: nil, completionHandler: nil)
-        
-        // 再生する
-        self.audioPlayerNode.play()
         pushController(withName: "Ouenn", context: nil)
-        
     }
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         // Configure interface objects here.
+        getImage()
     }
     
     override func willActivate() {
         // アプリが起動している時の動作
         super.willActivate()
+        //画像取得に関するもの
+        isActive = true
         //充電時の画面遷移
         if WKInterfaceDevice.current().batteryState == WKInterfaceDeviceBatteryState.charging{
             //NoAの寝てる画像に移動
@@ -71,16 +70,17 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // アプリが起動してない状態の動作
         super.didDeactivate()
-        
+        //画像取得に関するもの
+        isActive = false
+        if let t = task {
+            if t.state == URLSessionTask.State.running {
+                t.cancel()
+            }
     }
     
-    
+    }
     @IBAction func outSound() {
-        musicSet(name:"se2", type:"wav")
-        // オーディオファイルの再生をスケジュールする
-        self.audioPlayerNode.scheduleFile(self.audioFile!, at: nil, completionHandler: nil)
-        // 再生する
-        self.audioPlayerNode.play()
+       getImage()
     }
     //音楽をながす関数の作成
     func musicSet (name:String, type:String){
@@ -102,5 +102,25 @@ class InterfaceController: WKInterfaceController {
             fatalError("\(error)")
         }
     }
-    
+    //リモートの画像取得
+    func getImage() -> Void {
+        let url = URL(string:"https://s3-ap-northeast-1.amazonaws.com/noastorage/noa.png")!
+        let conf = URLSessionConfiguration.default
+        let session = URLSession(configuration: conf)
+        task = session.dataTask(with: url) { (data, res, error) -> Void in
+            if let e = error {
+                print("dataTaskWithURL fail: \(e.localizedDescription)")
+                return
+            }
+            if let d = data {
+                let image = UIImage(data: d)
+                DispatchQueue.main.async(execute: {
+                    if self.isActive {
+                        self.NoA.setImage(image)
+                    }
+                })
+            }
+        }
+        return task!.resume()
+    }
 }
