245db2de999d37423402ba9087c7cc5045745176
ka-kun <39756837+ka-kun@users.noreply.github.com>
2018-12-03 15:28:20 +0900

    動画画面背景の変更

--- Project_NoA/Project_NoA/ViewController/MovieViewController.swift
+++ Project_NoA/Project_NoA/ViewController/MovieViewController.swift
@@ -15,10 +15,6 @@ class MovieViewController: UIViewController {
     var audioPlayer: AVAudioPlayer!
     let userDefaults = UserDefaults.standard
     var myAp = UIApplication.shared.delegate as! AppDelegate
-    //応援ボタン
-    @IBAction func ouenButton(_ sender: UIButton) {
-        playSound(name: "sousaon")
-    }
     //吸入ボタン
     @IBAction func breathe(_ sender: UIButton) {
         moviePlay(movieURL: "https://s3-ap-northeast-1.amazonaws.com/noastorage/NoA%E5%90%B8%E5%85%A5%E9%9F%B3%E3%81%82%E3%82%8Av1.mp4")
