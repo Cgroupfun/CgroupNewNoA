a9447ca8ce186f8561e199e2cf162f5f577b307c
ka-kun <39756837+ka-kun@users.noreply.github.com>
2018-12-05 18:36:33 +0900

    競合修正

--- Project_NoA/Project_NoA/ViewController/ViewController.swift
+++ Project_NoA/Project_NoA/ViewController/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import AWSDynamoDB
+import AWSCore
 
 class ViewController: UIViewController {
     
@@ -61,6 +63,29 @@ class ViewController: UIViewController {
         "vc1","vc2","vc3","vc4","vc5","vc6","vc7","vc8","vc9","vc10","vc11","vc12"
     ]
     
+    var NoAState = NoAClass()
+    
+    func NoAcoin_data(){
+        let newsItem = NoAClass()
+        newsItem!.ID = "NoACoin"
+        
+        let dynamoDbObjectMapper = AWSDynamoDBObjectMapper.default()
+        
+        dynamoDbObjectMapper.load(
+            NoAClass.self,
+            hashKey: newsItem!.ID as Any,
+            rangeKey: nil,
+            completionHandler: {
+                (objectModel: AWSDynamoDBObjectModel?, error: Error?) -> Void in
+                if let error = error {
+                    print("Amazon DynamoDB Read Error: \(error)")
+                    return
+                }
+                self.NoAState = (objectModel as! NoAClass)
+                self.myAp.noaCoin = self.NoAState!.Coin
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //永続データの初期値
@@ -90,6 +115,7 @@ class ViewController: UIViewController {
         bg.layer.zPosition = -1
         self.view.addSubview(bg)
         
+        NoAcoin_data()
         NoA_Image()
     }
     
